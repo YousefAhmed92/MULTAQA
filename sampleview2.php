3920668a71a76788cf591e7bda6d11a2a5dc8e60
Yousef Ahmed Ibrahim <125690867+YousefAhmed92@users.noreply.github.com>
2024-09-02 11:05:55 +0300

    Update sampleview2.php

--- sampleview2.php
+++ sampleview2.php
@@ -57,7 +57,7 @@ $runselect=mysqli_query($connect,$select);
     </head>
     <body>
    <!-- NAV BAR STARTS -->
-    <nav id="NAV" class="navbar navbar-expand-lg navbar-dark fixed-top navbar-scrolled">
+       <nav id="NAV" class="navbar navbar-expand-lg navbar-dark fixed-top navbar-scrolled">
         <div class="container">
         <img src="img/image.png" alt="">
             <a class="navbar-brand" href="#">MULTAQA</a>
@@ -68,13 +68,14 @@ $runselect=mysqli_query($connect,$select);
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item mx-4">
-                        <a class="nav-link active" aria-current="page" href="#">Landing</a>
+                        <a class="nav-link active" aria-current="page" href="landing.php">Landing</a>
                     </li>
                     <li class="nav-item mx-4">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="client-send-request.php">my requests</a>
                     </li>
+                    
                     <li class="nav-item mx-4">
-                        <a class="nav-link" href="#">Profile</a>
+                        <a class="nav-link" href="freelancer.profile.php">my profile</a>
                     </li>
                 </ul>
                 <!-- <form class="d-flex" role="search">
@@ -83,7 +84,7 @@ $runselect=mysqli_query($connect,$select);
                 </form> -->
             </div>
         </div>
-    </nav>
+  </nav>
     <!-- NAV BAR ENDS -->
     <div class="main-content">
 <?php foreach($runselect as $data){?>
